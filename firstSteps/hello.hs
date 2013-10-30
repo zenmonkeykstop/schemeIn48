f373c124c71f7230a329a1f5ad0f6708f8546ca9
Kevin O Gorman <kevin.ogorman@gmail.com>
2013-10-29 23:47:09 -0400

    Escercise 2: Change the program so it performs a simple arithmetic operation on the two arguments and prints out the result. You can use read to convert a string to a number, and show to convert a number back into a string. Play around with different operations.

--- firstSteps/hello.hs
+++ firstSteps/hello.hs
@@ -3,4 +3,4 @@ import System.Environment
 
 main :: IO ()
 main = do args <- getArgs
-          putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
+          putStrLn ("Hello, " ++ show (read (args !! 0) + read(args !! 1)))
